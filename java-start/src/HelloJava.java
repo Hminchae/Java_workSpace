a559e20f082f8b9e43f27171caf5c3b1edf15150
Hminchae <hminchae@gmail.com>
2024-08-11 21:17:09 +0900

    자바 입문 - 변수

---
+++ java-start/src/HelloJava.java
@@ -0,0 +1,8 @@
+public class HelloJava {
+
+    public static  void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
+
+// 자바 언어는 대소문자를 구분한다. 대소문자가 다르면 오류가 발생할 수 있음
---
+++ java-start/src/variable/ex/VarEx3Question.java
@@ -0,0 +1,16 @@
+package variable.ex;
+
+public class VarEx3Question {
+
+    public static void main(String[] args) {
+        long longNum;
+        longNum = 1000000000000L;
+
+        System.out.println(longNum);
+
+        boolean boolValue;
+        boolValue = true;
+
+        System.out.println(boolValue);
+    }
+}
